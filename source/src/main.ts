1c9142db6934c89ee1fe4b820fa7e218d839eb9b
Thanawat Phumpuang <thanawat@opsta.co.th>
2023-05-25 11:43:02 +0700

    edit main and dockerfile

--- source/src/main.ts
+++ source/src/main.ts
@@ -8,8 +8,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter(),
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
